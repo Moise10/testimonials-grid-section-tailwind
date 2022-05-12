6dc6e19388a68c08d08a7bec1af320819375e6f5
moise <mbakopmoise237@gmail.com>
2022-05-12 23:02:50 +0800

    completed the media queries

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  content: ["./dist/index.html"],
-  theme: {
-    extend: {
-      container: {
-        padding: "6rem"
-      }
-    },
-  },
-  plugins: [],
-}
+	content: ['./dist/index.html'],
+	theme: {
+		extend: {
+			container: {
+				padding: '6rem',
+			},
+		},
+	},
+	plugins: [],
+};
